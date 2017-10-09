23c12697be7829a36dff42750f6a3bff67c92b96
RodrigoNdeC <rodrigondec@gmail.com>
2017-10-09 00:08:15 -0300

    main ok

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from Conta import Conta
 from Banco import Banco
 from Pessoa import Pessoa
 import logging
 
 
-qt_pessoas = 5
+qt_pessoas = 4
 
-banco = Banco()
+Banco()
 
 for _ in range(0, qt_pessoas):
     Pessoa()
