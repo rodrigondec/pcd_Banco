cb36b6aa74612eba1483bdf490d0cd69a712ea05
RodrigoNdeC <rodrigondec@gmail.com>
2017-10-08 22:37:46 -0300

    banco com contas e operações de conta

--- Banco.py
+++ Banco.py
@@ -3,6 +3,8 @@ from Logger import Log
 from time import sleep
 from Caixa import Caixa
 from Conta import Conta
+from contextlib import ExitStack
+from Exceptions import TransfException
 
 class Banco(object):
     """banco"""
@@ -15,83 +17,65 @@ class Banco(object):
         return type._instance
 
     def init(self):
-        self.contas = {}
-
-        self.dinheiro_investimento = Event()
-        self.dinheiro_investimento.clear()
-
-        self.operacao_liberada = Event()
-        self.operacao_liberada.set()
-
         self.t = Thread(target=self.investimento, name='Banco_Investimento')
-        # self.t.start()
+        self.t.start()
 
     def __str__(self):
         return "Banco"
 
     def investimento(self):
+        sleep(10)
         while True:
-            Banco.log.info("pergunta, tenho dinheiro pra investir?")
-            if not self.dinheiro_investimento.is_set():
-                Banco.log.info("esperando condicao para investir")
-                Banco().dinheiro_investimento.wait()
-            with self.l_dinheiro:
+            try:
+                Banco.log.info("investimento iniciando")
+                for id_pessoa, conta  in Conta.contas.items():
+                    conta.lock.acquire()
 
-                Banco.log.info("investindo o dinheiro dos clientes. Bloqueando operacoes de saque e "
-                                "transferencia")
-                self.operacao_liberada.clear()
+                sleep(5)
 
-                self.dinheiro = 0
-                for id_pessoa, valor in self.clientes.items():
-                    self.clientes[id_pessoa] = 0
-                    self.clientes_investimento[id_pessoa] = valor
-
-                sleep(8)
-
-                for id_pessoa, valor in self.clientes_investimento.items():
-                    self.dinheiro += valor + valor / 10
-                    self.clientes[id_pessoa] += valor + valor / 10
-                    self.clientes_investimento[id_pessoa] = 0
-
-                Banco.log.info("terminou de investir")
-                self.operacao_liberada.set()
+                for id_pessoa, conta in Conta.contas.items():
+                    conta.depositar(conta.saldo()/10)
+                Banco.log.info("investimento terminado")
+            finally:
+                for id_pessoa, conta  in Conta.contas.items():
+                    conta.lock.release()
 
             sleep(15)
 
-    def criar_conta(self, id_pessoa):
-        self.contas[id_pessoa] = Conta(id_pessoa)
+    def criar_conta(self, pessoa):
+        Conta(pessoa.get_id())
 
-    def saldo(self, id_pessoa):
-        return self.clientes[id_pessoa]
+    def saldo(self, pessoa):
+        return Conta.ver_saldo(pessoa.get_id())
 
-    def verif_saldo_suf(self, id_pessoa, valor):
-        if self.saldo(id_pessoa) < valor:
-            raise Exception('Saldo Insuficiente!')
-        return True
+    def sacar(self, pessoa, valor):
+        Caixa.fila.put(pessoa)
+        Conta.realizar_saque(pessoa.get_id(), valor)
 
-    def sacar(self, id_pessoa, valor):
-        self.verif_saldo_suf(id_pessoa, valor)
-        with self.l_dinheiro:
-            self.clientes[id_pessoa] -= valor
-            self.dinheiro -= valor
-            Banco.log.info("deu " + str(valor) + " para a Pessoa " + str(id_pessoa) + ". Ele tem agora " + str(
-                self.dinheiro))
-            if self.dinheiro < 100:
-                self.dinheiro_investimento.clear()
-        return valor
+    def depositar(self, pessoa, valor):
+        Caixa.fila.put(pessoa)
+        Conta.realizar_deposito(pessoa.get_id(), valor)
 
-    def depositar(self, id_pessoa, valor):
-        with self.l_dinheiro:
-            self.clientes[id_pessoa] += valor
-            self.dinheiro += valor
-            Banco.log.info("recebeu "+str(valor)+" da Pessoa "+str(id_pessoa)+". Ele tem agora "+str(self.dinheiro))
-            if self.dinheiro >= 100:
-                self.dinheiro_investimento.set()
-        return True
+    def transferir(self, pessoa_o, pessoa_d, valor):
+        Caixa.fila.put(pessoa_o)
 
-    def transferir(self, id_pessoa_o, id_pessoa_d, valor):
-        self.verif_saldo_suf(id_pessoa_o, valor)
-        with self.l_dinheiro:
-            self.sacar(id_pessoa_o, valor)
-            self.depositar(id_pessoa_d, valor)
-        return valor
+        conta_o = Conta.get_conta(pessoa_o.get_id())
+        conta_d = Conta.get_conta(pessoa_d.get_id())
+
+        try:
+            conta_o.lock.acquire()
+            conta_d.lock.acquire()
+
+            saldo_o_antes = conta_o.saldo()
+            saldo_d_antes = conta_d.saldo()
+
+            conta_o.sacar(valor)
+            conta_d.depositar(valor)
+
+            if conta_o.saldo() != (saldo_o_antes-valor) or conta_d.saldo() != (saldo_d_antes-valor):
+                conta_o.dinheiro = saldo_o_antes
+                conta_d.dinheiro = saldo_d_antes
+                raise TransfException()
+        finally:
+            conta_o.lock.release()
+            conta_d.lock.release()
