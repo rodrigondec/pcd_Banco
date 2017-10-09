9914430af2c8f3b9d16d97641cffb0b47b7894eb
RodrigoNdeC <rodrigondec@gmail.com>
2017-10-09 00:07:13 -0300

    caixa com thread funcionando

--- Caixa.py
+++ Caixa.py
@@ -8,23 +8,27 @@ class Caixa(object):
 
     log = Log("caixa")
     id_caixa = 0
+    caixas = []
     fila = Queue()
 
     def __init__(self):
         Caixa.id_caixa += 1
+        Caixa.caixas.append(self)
         self.id_caixa = str(Caixa.id_caixa)
-        self.lock = Lock()
+
         self.thread = Thread(target=self.run, name=('Caixa ' + self.id_caixa))
+        self.thread.start()
 
     def __str__(self):
         return self.id_caixa
 
     def run(self):
         while True:
-            Caixa.log.info("espera fila")
             pessoa = Caixa.fila.get()
-            Caixa.log.info(pessoa + " está usando o caixa")
+
+            Caixa.log.info("{} esta usando o caixa".format(pessoa))
             pessoa.vez.set()
-            Caixa.log.info("espera " + pessoa + " terminar de usar")
+
             if not pessoa.uso_caixa.is_set():
+                Caixa.log.info("espera {} terminar de usar".format(pessoa))
                 pessoa.uso_caixa.wait()
