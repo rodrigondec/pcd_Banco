6bcc3969089aff603cbd89dc2d397f7a9a11df90
RodrigoNdeC <rodrigondec@gmail.com>
2017-10-08 22:36:53 -0300

    coleção de contas na classe Conta

--- Conta.py
+++ Conta.py
@@ -1,9 +1,15 @@
 from threading import RLock
+from Exceptions import SaldoException
 
 class Conta(object):
     """Conta"""
 
+    contas = {}
+
     def __init__(self, id_pessoa):
+        assert isinstance(id_pessoa, str)
+
+        Conta.contas[id_pessoa] = self
         self.id_pessoa = id_pessoa
         self.dinheiro = 0
         self.lock = RLock()
@@ -17,15 +23,34 @@ class Conta(object):
 
     def _verificar_saldo_suf(self, valor):
         if self.saldo() < valor:
-            raise Exception('Saldo Insuficiente!')
-        return True
+            raise SaldoException("tem na conta {saldo}. faltam {saque} para sacar {valor}".format(saldo=self.saldo(), saque=valor-self.saldo(), valor=valor))
 
     def sacar(self, valor):
         with self.lock:
             self._verificar_saldo_suf(valor)
             self.dinheiro -= valor
-            return True
 
     def depositar(self, valor):
         with self.lock:
             self.dinheiro += valor
+        return
+
+    @staticmethod
+    def realizar_saque(id_pessoa, valor):
+        assert isinstance(id_pessoa, str)
+        Conta.contas[id_pessoa].sacar(valor)
+
+    @staticmethod
+    def realizar_deposito(id_pessoa, valor):
+        assert isinstance(id_pessoa, str)
+        Conta.contas[id_pessoa].depositar(valor)
+
+    @staticmethod
+    def ver_saldo(id_pessoa):
+        assert isinstance(id_pessoa, str)
+        return Conta.contas[id_pessoa].saldo()
+
+    @staticmethod
+    def get_conta(id_pessoa):
+        assert isinstance(id_pessoa, str)
+        return Conta.contas[id_pessoa]
